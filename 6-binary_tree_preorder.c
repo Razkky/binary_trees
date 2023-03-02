9471b5638e1b76f3fd4c89bd7224cb08b0469e28
abdulkarim <abdulkarimkrim6789@gmail.com>
2023-03-02 08:50:34 +0100

    fix betty style

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 /**
- * binary_tree_preoder - goes through a tree using pre_order traversal
+ * binary_tree_preorder - goes through a tree using pre_order traversal
  * @tree: pointer ato the root node of the tree to traverse
- * @func: a pointer to a fucntion to call for each node
+ * @fucn: a pointer to a fucntion to call for each node
  * if tree or func is NULL do nothing
  */
 
