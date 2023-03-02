cb352c726c7bc8df04ad5ccc9ee10bb0ec65bdb5
abdulkarim <abdulkarimkrim6789@gmail.com>
2023-03-02 16:49:09 +0100

    add description for return value

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,7 +2,7 @@
 /**
  * binary_tree_height - measures the height of a binary tree
  * @tree: pointer to the root node of the tree
- * return: height
+ * Return: height
  */
 
 size_t binary_tree_height(const binary_tree_t *tree)
