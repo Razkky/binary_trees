730a85000ee051371f82bf7b4ff8f3fa3be5bdd6
abdulkarim <abdulkarimkrim6789@gmail.com>
2023-03-02 16:46:57 +0100

    add an if statement

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,12 +7,11 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_h, right_h;
+	size_t left_h = 0, right_h = 0;
 
 	if (!tree)
-		return (-1);
-	left_h = 1 + binary_tree_height(tree->left);
-	right_h = 1 + binary_tree_height(tree->right);
-
+		return (0);
+	left_h = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right_h = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 	return (left_h > right_h ? left_h : right_h);
 }
