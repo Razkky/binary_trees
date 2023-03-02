ee90f1651876e26503e184c93d64f2896566326b
abdulkarim <abdulkarimkrim6789@gmail.com>
2023-03-02 10:56:53 +0100

    postorder trasversal

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_postorder - goes through a binary tree in post_order trasversal
+ * @tree: pointer to the root node of a tree
+ * @func: pointer to a fucntion to print the data of the node
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
