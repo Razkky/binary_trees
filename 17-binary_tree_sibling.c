e5fd053bd2b49b307d0b09f493d966dded526e11
abdulkarim <abdulkarimkrim6789@gmail.com>
2023-03-03 10:26:46 +0100

    check if sibling is null

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -15,5 +15,8 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 
 	if (node->parent->left->n == node->n)
 		return (node->parent->right);
+	if (node->parent->right == NULL || node->parent->left == NULL)
+		return (NULL);
+
 	return (NULL);
 }
