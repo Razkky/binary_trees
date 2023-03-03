c507811889fbde96fc2646899972e1de2ee8709f
abdulkarim <abdulkarimkrim6789@gmail.com>
2023-03-03 10:22:29 +0100

    check for sibling of a node

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_sibling - find the siblings of a node
+ * @node: pointer to the node of a tree
+ * Return: a pointer to the node sibling
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+
+	if (node->parent->right->n == node->n)
+		return (node->parent->left);
+
+	if (node->parent->left->n == node->n)
+		return (node->parent->right);
+	return (NULL);
+}
